76cf273fc2305f180b0671b5bbecb2db69ada077
Isaiah G <isaiahrg12@gmail.com>
2022-07-31 19:44:50 -0500

    Could not comment. Fixed it

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -55,7 +55,7 @@ const Detail = ({ postDetails }: IProps) => {
     }
   };
 
-  const addComment = async (e) => {
+  const addComment = async (e: { preventDefault: () => void }) => {
     e.preventDefault();
 
     if (userProfile && comment) {
